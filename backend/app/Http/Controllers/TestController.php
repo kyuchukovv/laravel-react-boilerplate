7099896b5c6ac91458078c89e7b1ba8113b69aa2
vlk <vlk@apacta.com>
2022-11-03 17:17:11 +0200

    Setting environment

---
+++ backend/app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        return response([
+            [
+                'id' => 1,
+            ],
+            ['id' => 2]
+        ]);
+    }
+}
